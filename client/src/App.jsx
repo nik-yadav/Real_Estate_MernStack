db9a7629fc53129bae420c57c7fa8c24ae458a4e
Nikhil Yadav <132257962+nik-yadav@users.noreply.github.com>
2023-11-14 12:45:34 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import Home from './pages/Home'
+import About from './pages/About'
+import Header from './Components/Header'
+function App() {
+
+  return (
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
