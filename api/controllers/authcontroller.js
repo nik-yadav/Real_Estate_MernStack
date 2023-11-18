dbca4a78618f4ec50fc500142f9adfaba362b8af
Nikhil Yadav <132257962+nik-yadav@users.noreply.github.com>
2023-11-18 10:30:32 +0530

    Update authcontroller.js

--- api/controllers/authcontroller.js
+++ api/controllers/authcontroller.js
@@ -1,15 +1,40 @@
-import User from '../UserModal/UserModel.js'
-import bcrypt from 'bcryptjs'
+import User from "../UserModal/UserModel.js";
+import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10); // changed 'hashPassword' to 'password'
+  const newUser = new User({ username, email, password: hashedPassword }); // use 'hashedPassword' instead of 'password'
+  try {
+    await newUser.save();
+    res.status(201).json("User Created Sucessfully");
+  } catch (error) {
+    next(error);
+  }
+};
 
-    const {username, email, password} = req.body;
-    const hashedPassword = bcrypt.hashSync(password, 10) // changed 'hashPassword' to 'password'
-    const newUser = new User({username, email, password: hashedPassword}) // use 'hashedPassword' instead of 'password'
-    try {
-        await newUser.save();
-        res.status(201).json("User Created Sucessfully");   
-    } catch (error) {
-        res.status(500).json(error)
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler404, "User not found");
     }
-}
+    const validPass = bcrypt.compareSync(password, validUser.password);
+    if (!validPass) return next(errorHandler(401, "Wrong Credentials!"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_secret);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
